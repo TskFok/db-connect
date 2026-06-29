f4f5b15eb83a39c52dabc3c065222d609e01a37b
杨毅 <yi.yang@ushopal.com>
2026-06-29 13:32:13 +0800

    发布 v1.0.3

--- src/appVersion.ts
+++ src/appVersion.ts
@@ -4,5 +4,5 @@ declare const __APP_VERSION__: string | undefined;
 export function getAppVersion(): string {
   return typeof __APP_VERSION__ === "string" && __APP_VERSION__.length > 0
     ? __APP_VERSION__
-    : "1.0.2";
+    : "1.0.3";
 }
